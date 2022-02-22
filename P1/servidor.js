465aa03fed45894ad5d2f925289a6d2b2153b5ab
iperezl2017 <i.perezl.2017@alumnos.urjc.es>
2022-02-22 20:21:24 +0100

    limpieza js

--- P1/servidor.js
+++ P1/servidor.js
@@ -1,44 +1,25 @@
-//-- Importo los modulos http, fs y url
+
 const http = require('http');
 const fs = require('fs');
-
-//-- URL no hace falta ponerla
-//--const URL = require('url');
-
-//-- Definir el puerto a utilizar
 const PUERTO = 7007; 
 
-//-- Crear el servidor
 const server = http.createServer(function(req, res) {
-    
-  //-- Indicamos que se ha recibido una petición
-  console.log("\nPetición recibida!");
 
-  //-- Obtengo URL del mensaje de solicitud
+  console.log("\nPetición recibida!");
   let myURL = new URL(req.url, 'http://' + req.headers['host']);
   console.log("La URL del recurso solicitado es: " + myURL.href)
-
-    //-- Declaro variable para almacenar los recursos pedidos
   let solicitud = "";
-
-  //-- Analizo la peticion
-  if(myURL.pathname == '/') { //-- http://ip:port/
-    solicitud += "/web.html" //-- Pagina principal
-  } else if(myURL.pathname == "/favicon.icon"){
+  if(myURL.pathname == '/') { 
+    solicitud += "/web.html" 
+  }else if(myURL.pathname == "/favicon.icon"){
     filename = 'imagenes/ico.ico'
-  }else { // Otra peticion
+  }else {
     solicitud = myURL.pathname;
   }
-  
-  //-- Extraigo el recurso pedido y su extension
-  file_extension = solicitud.split(".")[1]; //-- Extension
-  solicitud = "." + solicitud //-- El punto es necesario
-
+  file_extension = solicitud.split(".")[1];
+  solicitud = "." + solicitud 
   console.log("Nombre del fichero: " + solicitud);
   console.log("Extension del recurso: " + file_extension);
-
-
-  //-- Declaracion de los tipo de mime
   const type_mime = {
     "html" : "text/html",
     "css" : "text/css",
@@ -48,33 +29,21 @@ const server = http.createServer(function(req, res) {
     "gif" : "image/gif",
     "ico" : "image/ico",
   }; 
-
-  //-- Extraigo el tipo mime
   let mime = type_mime[file_extension];
   console.log("El tipo mime asociado: " + mime);
-
-  //- Leer el fichero de manera sincrona
   fs.readFile(solicitud, function(err,data) {
-    //-- En caso de error (pagina no encuentrada)
     if(err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
       console.log("404 Not Found");
       solicitud = "html/error.html";
       data = fs.readFileSync(solicitud);
-       
-    }else { //-- En caso de NO dar error
+    }else {
       res.writeHead(200, {'Content-Type': mime});
       console.log("200 OK")
     }
-    
-    // Envio el tipo de fichero solicitado
     res.write(data);
     res.end();
   });
-   
 });
-  
-//-- Activo el servidor:
 server.listen(PUERTO);
-
-console.log("Server tienda activado!. Escuchando en puerto: " + PUERTO);
+console.log("Server activo!. Escuchando en puerto: " + PUERTO);
