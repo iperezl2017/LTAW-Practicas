7424b38d36ba3419d27fa33e9ab1f53433b30868
iperezl2017 <i.perezl.2017@alumnos.urjc.es>
2022-05-21 17:29:48 +0200

    comandos

--- P3/server.js
+++ P3/server.js
@@ -5,3 +5,13 @@ const colors = require('colors');
 const port = 9090;
 const tiempo = Date.now();
 const fecha = new Date(tiempo);
+
+let help_msg = ("Comandos: <br>" +
+    "--> <b>'/help' </b>: Mostrar todos los comandos<br>" +
+    "--> <b>'/list' </b>: Mostrar el numero de todos los usuarios conectados<br>" +
+    "--> <b>'/hello' </b>: Saludar al cliente<br>" +
+    "--> <b>'/date' </b>: Mostrar la fecha<br>");
+
+let list_msg = ("Usuarios conectados: ");
+let hello_msg = ("Saludos invocador");
+let date_msg = (fecha.toUTCString());
