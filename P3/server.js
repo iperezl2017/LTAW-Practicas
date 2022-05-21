e80e5073dbf87bfcab4b061bda17156772b5ade0
iperezl2017 <i.perezl.2017@alumnos.urjc.es>
2022-05-21 17:34:49 +0200

    funcion para comandos

--- P3/server.js
+++ P3/server.js
@@ -6,6 +6,7 @@ const port = 9090;
 const tiempo = Date.now();
 const fecha = new Date(tiempo);
 
+let users = 0;
 let help_msg = ("Comandos: <br>" +
     "--> <b>'/help' </b>: Mostrar todos los comandos<br>" +
     "--> <b>'/list' </b>: Mostrar el numero de todos los usuarios conectados<br>" +
@@ -15,3 +16,23 @@ let help_msg = ("Comandos: <br>" +
 let list_msg = ("Usuarios conectados: ");
 let hello_msg = ("Saludos invocador");
 let date_msg = (fecha.toUTCString());
+let error_msg = ("Comando erroneo");
+
+function commands(msg){
+    let body;
+    if(msg == '/help') {
+        body = help_msg;
+    }
+    else if(msg == '/list') {
+        body = list_msg + users;
+    }
+    else if(msg == '/help') {
+        body = hello_msg;
+    }
+    else if(msg == '/help') {
+        body = date_msg;
+    }
+    else{
+        body = error_msg;
+    }
+}
