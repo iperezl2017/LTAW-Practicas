37c284af59738dd9498bac4eb615ab8605564f30
iperezl2017 <i.perezl.2017@alumnos.urjc.es>
2022-05-23 12:05:55 +0200

    constantes iniciales

--- P4/main.js
+++ P4/main.js
@@ -1,1 +1,13 @@
 const socket = require('socket.io');
+const http = require('http');
+const express = require('express');
+const colors = require('colors');
+const electron = require('electron');
+const ip = require('ip');
+const process = require('process');
+const port = 9090;
+const time = Date.now();
+const fecha = new Date(tiempo);
+
+let users = 0;
+let win = null;
