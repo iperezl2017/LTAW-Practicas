021effc4d9eecd8123df33f148079e4f8e3081a0
iperezl2017 <i.perezl.2017@alumnos.urjc.es>
2022-05-23 12:56:48 +0200

    extraccion de datos

--- P4/index.js
+++ P4/index.js
@@ -1,4 +1,30 @@
 const electron = require('electron');
 const qrcode = require('qrcode');
 
-const v_node = document.getElementById(node)
+const v_node = document.getElementById("node");
+const v_chrome = document.getElementById("chrome");
+const v_electron = document.getElementById("electron");
+const arch = document.getElementById("arch");
+const plat = document.getElementById("plat");
+const dir = document.getElementById("dir");
+const users = document.getElementById("users");
+const dir_ip = document.getElementById("ip");
+const code = document.getElementById("qrcode");
+const button = document.getElementById("btn_test");
+const msgs = document.getElementById("display");
+
+
+electron.ipcRenderer.on('informacion', (event, message) => {
+    v_node.textContent = message[0];
+    v_chrome.textContent = message[1];
+    v_electron.textContent = message[2];
+    arch.textContent = message[3];
+    plat.textContent = message[4];
+    dir.textContent = message[5];
+    url = ("http://" + message[6] + ":" + message[7] + "/" + message[8]);
+    dir_ip.textContent = url;
+
+    qrcode.toDataURL(url, function(err,url){
+        code.src = url;
+    });
+});
